bdc10482d124fd078fc0de33f18c0bfaf5ce888a
CrystalGamma <dev@crystalgamma.de>
2018-08-19 14:15:02 +0200

    implement HTML5 outlining

--- script.js
+++ script.js
@@ -6,6 +6,33 @@
 		el.addEventListener('load', handler);
 		document.body.appendChild(el);
 	});
+	const outlineLevel = el => {
+		let ancestor = el.parentElement;
+		let level = +el.tagName.substring(1);
+		do {
+			if (ancestor.tagName == "SECTION") {level += 6;}
+			ancestor = ancestor.parentElement;
+		} while (ancestor != null);
+		console.log(level);
+		return level;
+	};
+	const treeMap = (flattenedTree, func, getLevel) => {
+		const out = [];
+		const hier = [];
+		console.log(flattenedTree);
+		for (let el of flattenedTree) {
+			const level = getLevel(el);
+			while (!!hier.length && hier[hier.length-1].level >= level) {
+				hier.pop();
+			}
+			const mapped = func(el, hier.length == 0 ? null : hier[hier.length-1].out);
+			if (!hier.length) {out.push(mapped);}
+			hier.push({level, out: mapped});
+			console.log(hier);
+		}
+		console.log(out);
+		return out;
+	}
 	// shamelessly use URL normalization to get a path relative to this script ☺
 	const baseDir = window.baseDir = document.currentScript.src + '/../';
 	loadScript(baseDir + 'jsonml.js').then(() => {
@@ -21,8 +48,19 @@
 		document.body.classList.add('enhanced');
 		fromJsonMl.nameSpaces['svg'] = 'http://www.w3.org/2000/svg';
 		fromJsonMl.nameSpaces['xlink'] = 'http://www.w3.org/1999/xlink';
-		document.body.appendChild(fromJsonMl(['header', ['h1', "CrystalGamma's Tech Blog"], ['', ['img', {src: baseDir + 'feed-icon.svg#main'}], "Feeds coming soon"]]));
-		document.body.appendChild(fromJsonMl(['nav.sidebar', "Outline goes here"]));
+		document.body.appendChild(fromJsonMl(['header', ['h1', "CrystalGamma's Tech Stuff"]]));//, ['', ['img', {src: baseDir + 'feed-icon.svg#main'}], "Feeds coming soon"]]));
+		let headerNumbering = 0;
+		document.body.appendChild(fromJsonMl(['nav.sidebar', ...treeMap([...container.querySelectorAll("h1,h2,h3,h4,h5,h5")], (el, parent) => {
+			headerNumbering += 1;
+			if (!el.id) {el.id = "heading" + headerNumbering;}
+			const res = ['li', ['a', {href: "#"+el.id}, el.textContent]];
+			console.log(parent);
+			if (parent) {
+				if (parent.length == 2) {parent.push(['ul']);}
+				parent[2].push(res);
+			}
+			return res;
+		}, outlineLevel)]));
 		document.body.appendChild(fromJsonMl(['.container', container]));
 	});
 })();
